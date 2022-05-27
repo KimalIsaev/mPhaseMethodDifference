0e67cba5d3e99d6c66e392e3c08ed09c090a84fb
KimalIsaev <kimchikumnik@gmail.com>
2022-05-27 03:32:47 +0300

    done

--- run.sh
+++ run.sh
@@ -21,35 +21,42 @@ alg_name_file=$(mktemp)
 im_name_file=$(mktemp)
 alg_path_file=$(mktemp)
 im_path_file=$(mktemp)
+im_max_orbit_size=$(mktemp)
 to_compare_file=$(mktemp)
 
-im_n=$1
-x_n=$2
-percision=$3
-test=$4
+im_n=999999
+test=$1
+sigma=$2
 
-echo $alg_run $alg_py $draw_py $x_n $percision $test
-$alg_run $alg_py $draw_py $x_n $percision $test > $alg_name_file
-$im_run $im_exe $draw_py $im_n $test > $im_name_file
-cat $alg_name_file
-sed 's,^,'$alg_p_dir/',; s,$,.txt,' $alg_name_file > $alg_path_file
+test_base=$(basename $test .csv)
+test_dir=$(dirname $test)
+tmp_test_dir=$parent_dir/'temp_tests'
+mkdir -p $tmp_test_dir
+test_file_for_alg=$tmp_test_dir/$test_base'.csv'
+
+echo $($im_run $im_exe $draw_py $sigma $im_n $test) > $im_name_file
 sed 's,^,'$im_p_dir/',; s,$,.txt,' $im_name_file > $im_path_file
-cat $alg_path_file
-paste -d' ' $alg_path_file $im_path_file > $to_compare_file
+while read im_name
+do
+	cat $im_name | wc -l >> $im_max_orbit_size
+done < $im_path_file
+paste -d' ' $im_max_orbit_size $test > $test_file_for_alg
 
-cat $to_compare_file
+echo $($alg_run $alg_py $draw_py $sigma $test_file_for_alg) > $alg_name_file
+sed 's,^,'$alg_p_dir/',; s,$,.txt,' $alg_name_file > $alg_path_file
+paste -d' ' $alg_path_file $im_path_file > $to_compare_file
 
 i=0
 while read line
 do
-	temp_name=$diff_dir/$(basename $test .csv)'_'$i'.png'
+	temp_name=$diff_dir/$test_base'_'$i'.png'
 	diff=$(python3 $kolmogorov_py $line $temp_name | tr -d \n) 
-	true_name=$diff_dir/$(basename $test .csv)'_'$i'_'$diff'.png'
+	true_name=$diff_dir/$test_base'_'$i'_'$diff'.png'
 	mv $temp_name $true_name
 	export i=$(($i+1))
 done < $to_compare_file
 
-rm $alg_name_file $im_name_file $alg_path_file $im_path_file $to_compare_file
+rm $alg_name_file $im_name_file $alg_path_file $im_path_file $to_compare_file $im_max_orbit_size $test_file_for_alg
 
 
 
